4b43c328d08333a26ebb04595aa70998ffaa5b1c
Raid <raidmnaufal8@gmail.com>
2025-05-27 23:06:17 +0700

    add image

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,69 +1,59 @@
-/*
-===============================================================================
-DDL Script: Membuat Bronze Tables untuk Data Warehouse Kapal
-===============================================================================
-Tujuan Script:
-    Script ini membuat tabel dalam skema 'bronze' untuk proyek "Perancangan-
-    Arsitektur-Data-Warehouse-untuk-Optimalisasi-Kinerja-Kapal-Berbasis-Big-
-    Data".
-===============================================================================
-*/
-
--- =========================
--- Raw Data Table
--- =========================
-
 USE ShipDataWarehouse;
 GO
 
+-- Drop tabel yang ada
 IF OBJECT_ID('bronze.raw_ship_data', 'U') IS NOT NULL
     DROP TABLE bronze.raw_ship_data;
 GO
 
+-- Buat tabel dengan urutan kolom yang sesuai CSV
 CREATE TABLE bronze.raw_ship_data (
-    id                      INT,
-    datetime_recorded       NVARCHAR(50),        -- Ubah dari DATETIME ke NVARCHAR dulu
-    mmsi                    NVARCHAR(100),       -- Perbesar ukuran
-    navigational_status     NVARCHAR(100),       -- Perbesar ukuran
-    sog                     NVARCHAR(50),        -- Ubah ke NVARCHAR dulu
-    cog                     NVARCHAR(50),
+    datetime_recorded       NVARCHAR(50),        -- Kolom pertama di CSV
+    id                      NVARCHAR(50),        -- Kolom kedua di CSV
+    mmsi                    NVARCHAR(100),
+    navigational_status     NVARCHAR(100),       -- NavigationalStatus
+    sog                     NVARCHAR(50),        -- SpeedOverGround_SOG
+    cog                     NVARCHAR(50),        -- CourseOverGround_COG
     heading                 NVARCHAR(50),
-    vessel_type             NVARCHAR(100),
+    vessel_type             NVARCHAR(100),       -- VesselType
     width                   NVARCHAR(50),
     length                  NVARCHAR(50),
     draught                 NVARCHAR(50),
-    rotation                NVARCHAR(50),
-    speed                   NVARCHAR(50),
-    rudder                  NVARCHAR(50),
-    wind_direction          NVARCHAR(50),
-    wind_speed_knots        NVARCHAR(50),
-    class_value             NVARCHAR(50),
-    vessel_sk               NVARCHAR(50),
-    time_sk                 NVARCHAR(50),
-    route_sk                NVARCHAR(50),
-    weather_sk              NVARCHAR(50),
-    position_sk             NVARCHAR(50),
-    sensor_sk               NVARCHAR(50),
-    navigation_sk           NVARCHAR(50),
-    fuel_consumption_liters NVARCHAR(50),
-    ship_speed_knots        NVARCHAR(50),
-    engine_temperature_c    NVARCHAR(50),
-    travel_distance_miles   NVARCHAR(50),
-    latitude                NVARCHAR(50),
-    longitude               NVARCHAR(50),
-    wave_height_meters      NVARCHAR(50),
-    air_temperature_c       NVARCHAR(50),
-    departure_port          NVARCHAR(200),
-    arrival_port            NVARCHAR(200),
-    route_name              NVARCHAR(200),
-    vessel_name             NVARCHAR(200),
-    imo_number              NVARCHAR(100),
-    flag                    NVARCHAR(100),
-    year_built              NVARCHAR(50),
-    gross_tonnage           NVARCHAR(50),
-    deadweight              NVARCHAR(50),
-    sensor_type             NVARCHAR(200),
-    sensor_status           NVARCHAR(100),
-    last_calibration_date   NVARCHAR(50)
+    rotation                NVARCHAR(50),        -- Rotation
+    speed                   NVARCHAR(50),        -- Speed
+    rudder                  NVARCHAR(50),        -- Rudder
+    wind_direction          NVARCHAR(50),        -- WindDirection
+    wind_speed_knots        NVARCHAR(50),        -- WindSpeed_Knots
+    class_value             NVARCHAR(50),        -- Class
+    vessel_sk               NVARCHAR(50),        -- Vessel_SK
+    time_sk                 NVARCHAR(50),        -- Time_SK
+    route_sk                NVARCHAR(50),        -- Route_SK
+    weather_sk              NVARCHAR(50),        -- Weather_SK
+    position_sk             NVARCHAR(50),        -- Position_SK
+    sensor_sk               NVARCHAR(50),        -- Sensor_SK
+    navigation_sk           NVARCHAR(50),        -- Navigation_SK
+    fuel_consumption_liters NVARCHAR(50),        -- FuelConsumption_Liters
+    ship_speed_knots        NVARCHAR(50),        -- ShipSpeed_Knots
+    engine_temperature_c    NVARCHAR(50),        -- EngineTemperature_C
+    travel_distance_miles   NVARCHAR(50),        -- TravelDistance_Miles
+    latitude                NVARCHAR(50),        -- Latitude
+    longitude               NVARCHAR(50),        -- Longitude
+    wave_height_meters      NVARCHAR(50),        -- WaveHeight_Meters
+    air_temperature_c       NVARCHAR(50),        -- AirTemperature_C
+    departure_port          NVARCHAR(200),       -- DeparturePort
+    arrival_port            NVARCHAR(200),       -- ArrivalPort
+    route_name              NVARCHAR(200),       -- RouteName
+    vessel_name             NVARCHAR(200),       -- VesselName
+    imo_number              NVARCHAR(100),       -- IMONumber
+    flag                    NVARCHAR(100),       -- Flag
+    year_built              NVARCHAR(50),        -- YearBuilt
+    gross_tonnage           NVARCHAR(50),        -- GrossTonnage
+    deadweight              NVARCHAR(50),        -- Deadweight
+    sensor_type             NVARCHAR(200),       -- SensorType
+    sensor_status           NVARCHAR(100),       -- Status
+    last_calibration_date   NVARCHAR(50)         -- LastCalibrationDate
 );
+GO
+
+PRINT 'Table bronze.raw_ship_data created with correct column order!';
 GO
